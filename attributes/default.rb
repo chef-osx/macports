2ee4face5014c2515176f468d15ada88dbf78cdb
Julian C. Dunn <jdunn@aquezada.com>
2015-03-22 20:19:32 -0400

    Update to MacPorts 2.3.3 for all platforms

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,27 +1,26 @@
 if node['platform_family'] == 'mac_os_x'
   case node['platform_version'].split('.')[1].to_i
   when 4
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.2.1-10.4-Tiger.dmg'
-    default['macports']['checksum'] = 'd171c5b3302bf1cb1c3122978fe260b5fd994e00ba12a434b649fa297c8f70cc'
+    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.4-Tiger.dmg'
+    default['macports']['checksum'] = '9c9a4c42b36f800936bce69d5c4bf6c41cec6f489d683bd96bd1d00847e1308d'
   when 5
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.2.1-10.5-Leopard.dmg'
-    default['macports']['checksum'] = '9bc34ff5d825d6092caac7320c9b99ab4a11dfb3f29f647570e226e83bf4a7ee'
+    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.5-Leopard.dmg'
+    default['macports']['checksum'] = 'f2cf82b62e1795b13021f4d52d5074639337e29face37ef1017d0dc616cc8d4f'
   when 6
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.2.1-10.6-SnowLeopard.pkg'
-    default['macports']['checksum'] = '5a573e129ef69980e50aed21903b0ff8a3d875701f2d74af45b264639d3ca48f'
+    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.6-SnowLeopard.pkg'
+    default['macports']['checksum'] = 'a76a827b982b93edf15cdb07ed11851052df67f0b4b792adf130e4dead1cf1e4'
   when 7
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.2.1-10.7-Lion.pkg'
-    default['macports']['checksum'] = 'fe231f18af67b404bf84effe8d479f22685db38a920e69b34d04f20aace3cc31'
+    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.7-Lion.pkg'
+    default['macports']['checksum'] = '990e4015749fae6132cdd450fb28b77095f4fb17f8622b54b5ecd4819f317c09'
   when 8
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.2.1-10.8-MountainLion.pkg'
-    default['macports']['checksum'] = 'd10bf4a27f89709501e1370d7d80f415eaf16bae23fd9ff3d4e96f86afdf8cd6'
+    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.8-MountainLion.pkg'
+    default['macports']['checksum'] = 'ef5ae9c9fbe1a84eb0b1a7b5bf7d4b6b0d763bb5739f84fce8b43e9e5c68d5a1'
   when 9
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.2.1-10.9-Mavericks.pkg'
-    default['macports']['checksum'] = '2df01bf88e1e3de32ada0f42a8a46fb992093baee62f9d911fa3ae3ee895d471'
+    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.9-Mavericks.pkg'
+    default['macports']['checksum'] = '30d35308cf3bf6b66e6ed489ae7d1012f6e465ec4b4349a5c26a959e9130b512'
   when 10
-    # Not yet officially supported, let's cheat and use the 10.9 one for now
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.2.1-10.9-Mavericks.pkg'
-    default['macports']['checksum'] = '2df01bf88e1e3de32ada0f42a8a46fb992093baee62f9d911fa3ae3ee895d471'
+    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.10-Yosemite.pkg'
+    default['macports']['checksum'] = '7eb27ccbdf5cb61840494d954e8b56a9ee96c250d6c6c6ac887f0d80836d2d17'
   else
     raise "Unsupported platform version #{node['platform_version']}"
   end
