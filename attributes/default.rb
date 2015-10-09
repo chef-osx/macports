8cf1232e06accbcc7b4ae3a9d00d03c552fac386
Julian C. Dunn <jdunn@chef.io>
2015-10-09 00:53:12 -0400

    Add El Capitan support; update all to 2.3.4

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,24 +4,27 @@ if node['platform_family'] == 'mac_os_x'
     default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.4-Tiger.dmg'
     default['macports']['checksum'] = '9c9a4c42b36f800936bce69d5c4bf6c41cec6f489d683bd96bd1d00847e1308d'
   when 5
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.5-Leopard.dmg'
-    default['macports']['checksum'] = 'f2cf82b62e1795b13021f4d52d5074639337e29face37ef1017d0dc616cc8d4f'
+    default['macports']['url'] = 'http://downloads.sourceforge.net/project/macports/MacPorts/2.3.4/MacPorts-2.3.4-10.5-Leopard.dmg'
+    default['macports']['checksum'] = 'd3aab3395fd20ffd4b40aaeaca63760821083ec57a8bba7a285a848c190b25bb'
   when 6
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.6-SnowLeopard.pkg'
-    default['macports']['checksum'] = 'a76a827b982b93edf15cdb07ed11851052df67f0b4b792adf130e4dead1cf1e4'
+    default['macports']['url'] = 'http://downloads.sourceforge.net/project/macports/MacPorts/2.3.4/MacPorts-2.3.4-10.6-SnowLeopard.pkg'
+    default['macports']['checksum'] = '76e8c60f0cf1c879cb111bcf621a4a8132abca7e21db1a0bad2eafa4489b16a6'
   when 7
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.7-Lion.pkg'
-    default['macports']['checksum'] = '990e4015749fae6132cdd450fb28b77095f4fb17f8622b54b5ecd4819f317c09'
+    default['macports']['url'] = 'http://downloads.sourceforge.net/project/macports/MacPorts/2.3.4/MacPorts-2.3.4-10.7-Lion.pkg'
+    default['macports']['checksum'] = '39c6c4f75543f8efa60f26fa67fa507cd8d72b0893a6382673f722c864ce8f25'
   when 8
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.8-MountainLion.pkg'
-    default['macports']['checksum'] = 'ef5ae9c9fbe1a84eb0b1a7b5bf7d4b6b0d763bb5739f84fce8b43e9e5c68d5a1'
+    default['macports']['url'] = 'http://downloads.sourceforge.net/project/macports/MacPorts/2.3.4/MacPorts-2.3.4-10.8-MountainLion.pkg'
+    default['macports']['checksum'] = '43c9b56dda623f897854d88c3f7747bd9ae4287fa12b16b1d5eb32f45417b2da'
   when 9
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.9-Mavericks.pkg'
-    default['macports']['checksum'] = '30d35308cf3bf6b66e6ed489ae7d1012f6e465ec4b4349a5c26a959e9130b512'
+    default['macports']['url'] = 'http://downloads.sourceforge.net/project/macports/MacPorts/2.3.4/MacPorts-2.3.4-10.9-Mavericks.pkg'
+    default['macports']['checksum'] = '4b0fd713e3887c5a0f2fa975f90b6818aeb35ec909931d0905c181085abef7f4'
   when 10
-    default['macports']['url'] = 'https://distfiles.macports.org/MacPorts/MacPorts-2.3.3-10.10-Yosemite.pkg'
-    default['macports']['checksum'] = '7eb27ccbdf5cb61840494d954e8b56a9ee96c250d6c6c6ac887f0d80836d2d17'
+    default['macports']['url'] = 'http://downloads.sourceforge.net/project/macports/MacPorts/2.3.4/MacPorts-2.3.4-10.10-Yosemite.pkg'
+    default['macports']['checksum'] = '453125fffb358c9547aab70fa39dc5404acc037c18d7b1d7488256e9e4374138'
+  when 11
+    default['macports']['url'] = 'http://downloads.sourceforge.net/project/macports/MacPorts/2.3.4/MacPorts-2.3.4-10.11-ElCapitan.pkg'
+    default['macports']['checksum'] = '8c0b492032c796d766b76da3d1fda8dea732a6cc9056633a3509e3ff06b5e8d8'
   else
-    raise "Unsupported platform version #{node['platform_version']}"
+    fail "Unsupported platform version #{node['platform_version']}"
   end
 end
